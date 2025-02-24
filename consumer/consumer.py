b16fe682566e0c2e031cf7616aa4d8c38f6910fd
tsofnat <tsofnat@n2ws.com>
2025-02-24 09:37:18 +0200

    exporter-deployment

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     # sleep a few seconds to allow RabbitMQ server to come up
-    sleep(5)
+    sleep(10)
     logging.basicConfig(level=logging.INFO)
     LOG = logging.getLogger(__name__)
     #credentials = pika.PlainCredentials('guest', 'guest')
